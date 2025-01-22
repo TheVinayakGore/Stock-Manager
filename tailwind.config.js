ef685b3f17457edb17de5f4bbe4b4e57db343231
Vinayak Gore <vvgore2675@gmail.com>
2025-01-22 19:29:25 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,4 +12,6 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
+
+export default config;
